41f34f32d16a6e0a3afd81daf6983cd65b0e07de
Aaron Myrold <amyrold@me.com>
2023-11-16 18:27:31 -0600

    Rough Script set up

---
+++ pimaML.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 18:25:01 2023
+
+@author: blue
+"""
+
+#%% Script outline
+
+
+
+
+#%% Data cleanup and normalization
+
+
+
+
+#%% Train/dev/test split
+
+
+
+
+#%% Model building
+
+
+
+
+#%% Evaluation
+
+
+
+
+
+
